e42e584c93d7a1302b25c437b29b8e300db9fad7
Stefan Schuhbaeck <stefan.schuhbaeck@hm.edu>
2020-08-24 14:24:28 +0200

    make convertX virtual

--- src/traci/API.h
+++ src/traci/API.h
@@ -20,8 +20,8 @@ public:
     virtual ~API() = default;
     using Version = std::pair<int, std::string>;
     Version getVersion() const;
-    TraCIGeoPosition convertGeo(const TraCIPosition&) const;
-    TraCIPosition convert2D(const TraCIGeoPosition&) const;
+    virtual TraCIGeoPosition convertGeo(const TraCIPosition&) const;
+    virtual TraCIPosition convert2D(const TraCIGeoPosition&) const;
 
     void connect(const ServerEndpoint&);
 };
